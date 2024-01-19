b76060758f08419dad23edefdfa7e601b3f649e2
Galina Moroz <kozinaksa@gmail.com>
2024-01-19 17:50:57 +0300

    feat: solve 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const path = require('path');
+const fileName = path.join(__dirname, 'text.txt');
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.setPrompt('Please, input some text> ');
+rl.prompt();
+
+rl.on('line', function (answer) {
+  if (answer.trim() === 'exit') rl.close();
+  fs.appendFile(fileName, answer + '\n', function (error) {
+    if (error) return console.log(error);
+  });
+  rl.prompt();
+}).on('close', function () {
+  console.log('Thanks. You wonderful!');
+  process.exit(0);
+});
